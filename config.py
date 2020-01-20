95f2463715ea831cd3960d26134535e28fc0caae
Shen <Ruofei.Shen@nike.com>
2020-01-20 15:23:09 +0800

    data master preparation

---
+++ config.py
@@ -0,0 +1,2 @@
+attribute_use_cols = ['STYLCOLOR_CD', 'STYLCOLOR_DESC', 'PE', 'GBL_CAT_SUM_DESC', 'RETL_GNDR_GRP', 'Classification']
+booking_use_cols = ['PLATFORM_DIMENSION', 'SEASON_DESC','STYLCOLOR_CD', 'or_booking', 'ar_booking']
